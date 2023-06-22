331b36eb3c50856bb5ffa773f7396d25e83dcb6d
Devfrank94 <francescomurro94@gmail.com>
2023-06-22 18:29:32 +0200

    create seeder ProjectsTecnologiesTable

--- database/seeders/ProjectsTecnologiesTableSeeder.php
+++ database/seeders/ProjectsTecnologiesTableSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Tecnology;
+use App\Models\Project;
 
 class ProjectsTecnologiesTableSeeder extends Seeder
 {
@@ -14,6 +16,16 @@ class ProjectsTecnologiesTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        for($i = 0; $i < 30; $i++){
+
+          //estraggo project random
+          $project = Project::inRandomOrder()->first();
+
+          // estraggo id tecnology random
+          $tecnology_id = Tecnology::inRandomOrder()->first()->id;
+          // dump($tecnology_id);
+
+          $project->tecnologies()->attach($tecnology_id);
+        }
     }
 }
